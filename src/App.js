134fe6f9f3fdd8dcea1a85ebc9777dd2210deb13
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-03-16 22:41:29 -0300

    Updated App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Form from './components/Form';
+import Header from './components/Header';
+import TodosList from './components/TodosList';
 
-function App() {
-  return (
-    <div className="App">
-        APP
-    </div>
-  );
-}
+const App = () => {
+    const initialState = JSON.parse(localStorage.getItem('todos')) || []
+
+    const [input, setInput] = useState('');
+    const [todos, setTodos] = useState(initialState);
+    const [editTodo, setEditTodo] = useState(null)
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
+
+    return (
+        <div className="container">
+            <div className="app-wrapper">
+                <div className="">
+                    <Header />
+                </div>
+                <Form
+                    input={input}
+                    setInput={setInput}
+                    todos={todos}
+                    setTodos={setTodos}
+                    editTodo={editTodo}
+                    setEditTodo={setEditTodo}
+                />
+                <TodosList
+                    todos={todos}
+                    setTodos={setTodos}
+                    editTodo={editTodo}
+                    setEditTodo={setEditTodo}
+                />
+            </div>
+        </div>
+    );
+};
 
 export default App;
