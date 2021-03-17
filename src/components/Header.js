9f438c1c6fe47e1417a6f90c49a3f94e40fe0020
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-03-16 22:40:45 -0300

    Added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>TODO LIST</h1>
+        </div>
+    )
+}
+
+export default Header
